4e1d03f53e26888c299690698842ae2c430fd2c9
Noor <noor.non1993@gmail.com>
2023-09-07 02:08:14 +0300

    assert

--- cypress/e2e/almosafer.cy.js
+++ cypress/e2e/almosafer.cy.js
@@ -2,12 +2,12 @@
 
 describe('assertion for ', () => {
   it('currency is SAR', () => {
-    cy.visit('https://www.almosafer.com/ar')
+    cy.visit('https://www.almosafer.com')
     cy.get('.cta__saudi').click()
     cy.get('[data-testid="Header__CurrencySelector"]').invoke('text').should('include', 'SAR')
 
 let arab="العربية"
-    cy.get('[data-testid="Header__LanguageSwitch"]').should('have.text','English')
+    cy.get('[data-testid="Header__LanguageSwitch"]').should('have.text',arab).click()
 
     cy.get('#uncontrolled-tab-example-tab-flights ').should('have.class','nav-item nav-link active')
 
